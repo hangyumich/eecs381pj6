6add60ab09b5be9f9a4108dc4799c006add77201
Chen Luo <luochen@umich.edu>
2016-04-18 06:26:27 -0400

    final version. Ready to submit

--- project6/debug.sh
+++ project6/debug.sh
@@ -14,15 +14,15 @@ diff my.out views.out
 diff my.out torpedo.out
 
 ./p6exe < demo1_in.txt > my.out
-diff my.out S1_1.out
+diff my.out demo1_out.txt
 
 ./p6exe < demo2_in.txt > my.out
-diff my.out S2_1.out
+diff my.out demo2_out.txt
 
-./p6exe < S2_2.in > my.out
-diff my.out S2_2.out
+./p6exe < demo3_in.txt > my.out
+diff my.out demo3_out.txt
 
-./p6exe < M4_in.txt > my.out
-diff my.out M4_out.txt
+# ./p6exe < demo4_in.txt > my.out
+# diff my.out demo4_out.txt
 
 valgrind --tool=memcheck --leak-check=full ./p6exe < status.in > my.out
